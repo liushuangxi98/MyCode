8d6ac2e0b35c09689c92424d2415c01678ed54fd
‘liushuangxi98’ <1604030114@qq.com>
2023-06-25 21:57:51 +0800

    11

--- example/code/functional.py
+++ example/code/functional.py
@@ -7,18 +7,18 @@
 import torch
 from torch.nn import functional as F
 
-input = torch.tensor([[1,2,0,3,1],
-                      [0,1,2,3,1],
-                      [1,2,1,0,1],
-                      [5,2,3,1,1],
-                      [2,1,0,1,1]])
+input = torch.tensor([[1, 2, 0, 3, 1],
+                      [0, 1, 2, 3, 1],
+                      [1, 2, 1, 0, 1],
+                      [5, 2, 3, 1, 1],
+                      [2, 1, 0, 1, 1]])
 
-kernel = torch.tensor([[1,2,3],
-                       [0,1,0],
-                       [2,1,0]])
+kernel = torch.tensor([[1, 2, 3],
+                       [0, 1, 0],
+                       [2, 1, 0]])
 
-input = torch.reshape(input, (1,1,5,5))
-kernel = torch.reshape(kernel, (1,1,3,3))
+input = torch.reshape(input, (1, 1, 5, 5))
+kernel = torch.reshape(kernel, (1, 1, 3, 3))
 
 output1 = F.conv2d(input, kernel, stride=1)
 output2 = F.conv2d(input, kernel, stride=2)
