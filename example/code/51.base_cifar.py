01936f9584722a0de866c8f944b1dca34a627ce4
‘liushuangxi98’ <1604030114@qq.com>
2023-07-25 21:09:57 +0800

    11

--- example/code/51.base_cifar.py
+++ example/code/51.base_cifar.py
@@ -6,11 +6,23 @@
 # @Description : 添加描述
 import torchvision
 import matplotlib.pyplot as plt
+import time
+import torch.optim
+import torchvision
+from torch import nn
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torch.nn import Conv2d, MaxPool2d, Flatten, Linear, Sequential
 
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.feature_process = Sequential([])
 
-train_data = torchvision.datasets.CelebA('../../dataset/51.datasets',
-                                        # transform=torchvision.transforms.ToTensor(),
-                                         download=True)
+    def forword(self, x):
+        return self.feature_process(x)
+dataset_path = r'D:\file\python\MyCode\dataset\51.datasets'
+train_data = torchvision.datasets.CelebA(root=dataset_path, split='train',target_type='attr',transform=None, download=False)
 # test_data = torchvision.datasets.CIFAR100('../../dataset/51.datasets', train=False,
 #                                          #transform=torchvision.transforms.ToTensor(),
 #                                          download=True)
