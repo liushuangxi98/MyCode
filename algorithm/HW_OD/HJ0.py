4512bdb9cf27d7ebff06e9ef1969c0f6e5998aa8
liushuangxi98 <1604030114@qq.com>
2023-12-10 23:08:23 +0800

    1

--- algorithm/HW_OD/HJ0.py
+++ algorithm/HW_OD/HJ0.py
@@ -1,39 +1,25 @@
-N = 15
+# @Time   :2018/5/21
+# @Author :Yinxing
 
-f = [[0] * N for _ in range(N)]
+def tone_merge(tone):
+    n = len(tone)
+    if n <= 1: return 0
+    if n == 2: return sum(tone)  # 长度为2时，就是这两堆石头的和
 
+    dp = [[float('inf')] * n for _ in range(n)]  # 初始化dp
+    for i in range(n): dp[i][i] = 0
+    sm = [tone[0]]  # 记录 列表从0 到 n 的累加和
+    for i in range(1, n): sm.append(sm[i - 1] + tone[i])  # 累加求和
 
-# 预处理出最高位为j的i位的非下降的数的数量
-def init():
-    for i in range(10):f[1][i] = 1
-    for i in range(2, N):
-        for j in range(10):
-            # 有i位时，最高位为j时的非下降数数量为少一位时，如3位4开头最大下降数数量为2位4开头+5开头+++9开头的最大下降数数量之和
-            f[i][j] = sum([f[i - 1][k] for k in range(j, 10)])
+    for gap in range(1, n):  # i 到 j 的间隔
+        for i in range(n - gap):
+            j = i + gap  # 确定本次  i--j
+            tmp = sm[j] - [0, sm[i - 1]][i > 0]  # 获取第 i 到第 j 堆石子的总数量
+            for k in range(i, j):  # 选择i 到 j 的最优解
+                dp[i][j] = min(dp[i][j], dp[i][k] + dp[k + 1][j] + tmp)
 
-
-def dp(n):
-    # 特判0，有一个数满足条件
-    if not n: return 1
-    nums = []
-    # 处理出n的每一位，存于nums中
-    while n:
-        nums.append(n % 10)
-        n //= 10
-    res, last = 0, 0  # last存储上一层的数
-    # 从高到低枚举每一位
-    for i in range(len(nums) - 1, -1, -1):
-        x = nums[i]
-        if last > x: break  # 如果当前位能取到的最大的数小于上一层上界，则退出
-        # x_i不取上界
-        for j in range(last, x):
-            res += f[i + 1][j]
-        # 取上界
-        last = x
-        if not i: res += 1  # 特判最后一个数
-    return res
+    return dp[0][n - 1]
 
 
-init()
-l, r = map(int, input().split())
-print(dp(r) - dp(l - 1))
+tone = [186, 64, 35, 32, 103]
+print(tone_merge(tone))
